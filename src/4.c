7c7598445bc890a387ebe30a46b88a65444f012e
selenepre05 <selenepre05@gmx.com>
2025-03-07 08:59:52 +0000

    Add 4.c

---
+++ src/4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int age = 30;
+    if (age >= 18) {
+        printf("You are old enough to vote!\n");
+    } else {
+        printf("You are not yet old enough to vote.\n");
+    }
+    return 0;
+}
